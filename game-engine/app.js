2e89c2914a79839fffcc750c62b3fdaf3495a689
kmftcy <kmftangchaoyang@gmail.com>
2025-10-01 14:34:29 +0800

    support CORS at engine

--- game-engine/app.js
+++ game-engine/app.js
@@ -25,6 +25,19 @@ process.on('SIGINT', () => {
     process.exit(0);
 });
 
+// CORS middleware
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+    
+    if (req.method === 'OPTIONS') {
+        res.sendStatus(200);
+    } else {
+        next();
+    }
+});
+
 // Middleware: Request logging
 app.use((req, res, next) => {
     console.log(`${new Date().toISOString()} - ${req.method} ${req.url}`);
